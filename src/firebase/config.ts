97caff946b9851d720bacd753456a7bdf7dcfe81
AstorXD <79021539+AstorXD@users.noreply.github.com>
2025-05-29 11:41:54 -0300

    Configure Firebase Connection

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -11,5 +11,8 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
 export const db = getDatabase(app);
